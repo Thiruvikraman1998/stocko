ae352eb84efd83317660699bfc33c29bf71e13bc
Thiruvikraman1998 <development.thiru@gmail.com>
2024-02-25 19:36:29 +0530

    Added Theme constants

---
+++ stocko/android/app/src/main/kotlin/com/example/stocko/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stocko
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ stocko/lib/utils/theme_constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ThemeConstants {
+  static const Color primaryColor = Color(0xFF0663C7);
+  static const Color scaffoldBgColor = Color(0xFFFFFFFF);
+  static const Color blueGreyColor = Color(0xFFEEF5FC);
+  static const Color boldBlueGreyColor = Color(0xFFE6EDF2);
+  static const Color greenColor = Color(0xFF009262);
+  static const Color greenColor2 = Color(0xFF32C997);
+  static const Color boldGreenColor = Color(0xFF1B6E53);
+  static const Color lightGreenColor = Color(0xFFF1FBF8);
+  static const Color redColor = Color(0xFFE02B1D);
+  static const Color boldRedColor = Color(0xFFE01507);
+  static const Color lightRedColor = Color(0xFFFFF1F0);
+  static const Color borderGreyColor = Color(0xFF89939E);
+  static const Color boldGreyColor = Color(0xFF717171);
+  static const Color boldGreyColor2 = Color(0xFFD9D9D9);
+  static const Color blackColor = Color(0xFF000000);
+}
